c9b1cba7c0ff288695874ebda4882f03e67d9b97
pawelkosno <pawel.kosno95@gmail.com>
2025-01-06 17:10:10 +0100

    Add pseudo email send

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -37,3 +37,38 @@ tabs.forEach((tab) => {
 });
 
 /*=============== CONTACT FORM =============== */
+const contactForm = document.getElementById('contact-form'),
+  contactName = document.getElementById('contact-name'),
+  contactEmail = document.getElementById('contact-email'),
+  contactSubject = document.getElementById('contact-subject'),
+  contactMessage = document.getElementById('contact-message'),
+  errorMessage = document.getElementById('error-message');
+
+//Later attach react-hook-form yup and some Azure service
+const sendEmail = (e) => {
+  e.preventDefault();
+
+  // Check if the field has a value
+  if (
+    contactName.value === '' ||
+    contactEmail.value === '' ||
+    contactSubject.value === '' ||
+    contactMessage.value === ''
+  ) {
+    //show message
+    errorMessage.textContent = 'Write all the input fields';
+  } else {
+    console.log('Contact Name: ', contactName.value);
+    console.log('Contact Email: ', contactEmail.value);
+    console.log('Contact Subject: ', contactSubject.value);
+    console.log('Contact Message: ', contactMessage.value);
+
+    contactName.value = '';
+    contactEmail.value = '';
+    contactSubject.value = '';
+    contactMessage.value = '';
+    errorMessage.textContent = '';
+  }
+};
+
+contactForm.addEventListener('submit', sendEmail);
